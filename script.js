0bd48341411e7cefba82c72f75cbaeb58889d0bd
Fabricio-182 <marcofabri6@gmail.com>
2022-04-01 17:45:13 -0600

    feat: Add function to play as the computer

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Cool");
+//Function to choose randomly one of the three choices using an array
+function computerPlay() {
+    const values = ["Rock", "Paper", "Scissor"];
+    const finalValue = (values[Math.floor(Math.random() * values.length)])
+    console.log(Math.random())
+    alert(finalValue);
+
+}
